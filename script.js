25e64ebe2d9a0a6a1893ae8e8e47262891bc2fa3
ERENxJEAGER11 <114978438+ERENxJEAGER11@users.noreply.github.com>
2023-04-02 13:35:34 +0530

    debugging

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const task = ["aman", "zaheer"];
 const taskList = document.getElementsByClassName("task-list");
+const para = document.createElement("p");
+
+para.innerText = "This is a paragraph.";
+
+document.body.append(taskList.item);
+
+
+
+for (let i = 0; i < task.length; i++) {
+  const element = document.createElement("p");
+  element.innerText = task[i];
+  taskList.appendChild(element);
+}
 
 function adTask() {
   task.push(document.getElementById("my-task").value);
   console.log(task);
 }
-for (let i = 0; i < task.length; i++) {
-  const element = document.createElement("p");
-  element.textContent = task[i];
-  taskList.append(element);
-}
+
